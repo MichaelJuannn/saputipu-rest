53e19507dcea7a67c8f77adad47bd1ac3b3b6a39
MichaelJuannn <juan600d@gmail.com>
2023-12-13 15:28:09 +0700

    add login error status message

--- src/auth/dto/signIn.dto.ts
+++ src/auth/dto/signIn.dto.ts
@@ -1,6 +1,8 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { IsEmail, IsNotEmpty, IsString, Length } from 'class-validator';
 IsEmail();
 export class signInDto {
+  @ApiProperty({ example: 'user@domain.com' })
   @IsNotEmpty()
   @IsEmail()
   email: string;
